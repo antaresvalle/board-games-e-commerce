1189f19e2f2ab8145229a49391982e83a612aac4
antaresvalle <antaresvll561@gmail.com>
2018-08-09 02:02:56 -0500

    the Mercado Libre API enpoint is set as the parameter of the allProducts function to update the products depending on the enpoint of the category that is given to it. More styles are added and the paypal button is set

--- js/app.js
+++ js/app.js
@@ -1,30 +1,19 @@
-// $('#myModal').on('shown.bs.modal', function () {
-//     $('#myInput').trigger('focus')
-//   })
 $('.carousel').carousel()
 
-Vue.component('newP', {
-    props: ['image','title', 'price', 'id'],
-    template: `
-    <div class="col-sm-4 new-products" v-on:click="showDetails(id)">
-            <a href="#" data-toggle="modal" data-target="#exampleModal""><img :src="image" :alt="title"></a>
-            <a href="#" class="product-title"><p><strong> {{ title }} </strong></p></a>
-            <p>$ {{ price }}</p>
-    </div>
-    `,
-})
 
 Vue.component('product', {
-    props: ['image','title', 'price', 'id'],
+    props: ['image', 'title', 'price', 'id'],
     template: `
-    <div class="col-sm-4 new-products" v-on:click="showDetails(id)">
-            <a href="#" data-toggle="modal" data-target="#exampleModal""><img :src="image" :alt="title"></a>
-            <a href="#" class="product-title"><p><strong> {{ title }} </strong></p></a>
-            <p>$ {{ price }}</p>
+    <div class="col-sm-3 new-products" v-on:click="showDetails(id)">
+        <div class="card">
+            <a href="#" data-toggle="modal" data-target="#exampleModal" class="cont-img"><img :src="image" :alt="title" class="product-img"></a>
+            <p class="price">$ {{ price }}</p>
+            <a href="#" data-toggle="modal" data-target="#exampleModal" class="product-title"><p class="title"> {{ title }} </p></a>
+        </div>
     </div>
     `,
     methods: {
-        showDetails: function(item_id){
+        showDetails: function (item_id) {
             // console.log(item_id);
             $.ajax({
                 url: `https://api.mercadolibre.com/items/${item_id}`,
@@ -40,19 +29,15 @@ Vue.component('product', {
     }
 })
 
-
-
-
 var allProducts = new Vue({
     el: '#all-products',
     data: {
-        newProducts: [],
         products: [],
     },
     methods: {
-        loadProducts: function(){
+        loadProducts: function (endpoint) {
             $.ajax({
-                url: `https://api.mercadolibre.com/sites/MLA/search?q=juegos/mesa/#menu=categories`,
+                url: `https://api.mercadolibre.com/sites/MLA/search?q=${endpoint}`,
                 type: 'GET',
                 crossDomain: true,
                 datatype: 'json',
@@ -60,63 +45,80 @@ var allProducts = new Vue({
                 // console.log(response.results);
                 this.products = response.results;
             });
-        },
-        newP: function(){
-            $.ajax({
-                url: `https://api.mercadolibre.com/sites/MLA/search?q=juegos/mesa/_PublishedToday_YES`,
-                type: 'GET',
-                crossDomain: true,
-                datatype: 'json',
-            }).done((response) => {
-                console.log(response.results);
-                this.newProducts = response.results;
-            });
-        },
-        // showDetails: function(item_id){
-        //     console.log(item_id);
-        // }
+        }
     }
 })
 
 const displayModal = (data) => {
-    console.log(data)
-    // $('#modal').html('');
+
     let image = data.pictures[0].url;
     let modalItemTitle = data.title;
-    let template = `<div class="modal fade bd-example-modal-lg" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-    aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <img src="${image}">
-                <h5 class="modal-title" id="modalItemTitle">${modalItemTitle}</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
+    let modalPrice = data.price;
+    let template = `
+        <div class="modal fade bd-example-modal-lg" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+            aria-hidden="true">
+            <div class="modal-dialog modal-lg" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" onClick="clean();" data-dismiss='modal' class="close" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="row">
+                            <div class="col-8">
+                                <img src="${image}">
+                            </div>
+                            <div class="col-4">
+                                <p id="itemTitle"><strong>${modalItemTitle}</strong></p>
+                                <p class="price-modal">$${modalPrice}</p>
+                                <div id="description"></div>
+                                <div id="paypal-button"></div>
+                            </div>
+                         </div>
+                    </div>
+                </div>
             </div>
-            <div class="modal-body">
-                <p id="itemTitle"></p>
-                <div id="description"></div>
-            </div>
-            <div class="modal-footer">
+        </div>`
 
-            </div>
-        </div>
-    </div>
-</div>`
-$('#modal').append(template);
-$('#myModal').on('shown.bs.modal', function () {
-    $('#myInput').trigger('focus')
-  })
+    $('#modal-container').append(template);
+    $('#exampleModal').modal();
+
+    paypal.Button.render({
+        env: 'sandbox',
+        client: {
+            sandbox: 'demo_sandbox_client_id'
+        },
+        payment: function (data, actions) {
+            return actions.payment.create({
+                transactions: [{
+                    amount: {
+                        total: '0.01',
+                        currency: 'USD'
+                    }
+                }]
+            });
+        },
+        onAuthorize: function (data, actions) {
+            return actions.payment.execute()
+                .then(function () {
+                    window.alert('Thank you for your purchase!');
+                });
+        }
+    }, '#paypal-button');
 }
 
-const clean = () =>{
-    console.log('here');
-    // $('#modal').html('');
+const clean = () => {
+    $('#exampleModal').on('hidden.bs.modal', function (e) {
+        $('#exampleModal').modal('dispose');
+        $('#modal-container').html('');
+    })
 }
 
-allProducts.loadProducts();
-allProducts.newP();
+
+
+// allProducts.loadProducts();
+// allProducts.newP();
 // allProducts.showDetails("MLA673577112");
 
 // $(document).ready(() => {
